859255eee3ddea174d19f04aab13e7cb65b5d611
lamltph17490 <106149403+lamltph17490@users.noreply.github.com>
2022-12-15 16:12:22 +0700

    fixblog

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -9,16 +9,16 @@ type Props = {
 };
 
 const Blog = ({ blog }: Props) => {
-  return <div>
-    <Head>
-      <title>
-      {blog.title}
-      </title>
-    </Head>
-    <div className="mx-auto w-[1410px]">
-    {blog.content}
+  return (
+    <div>
+      <Head>
+        <title>{blog.title}</title>
+      </Head>
+      <div className="mx-auto w-[1410px]">
+        <div dangerouslySetInnerHTML={{ __html: blog.content }} />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -29,9 +29,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: "blocking",
   };
 };
-export const getStaticProps: GetStaticProps<Props> = async (
-  context: GetStaticPropsContext
-) => {
+export const getStaticProps: GetStaticProps<Props> = async (context: GetStaticPropsContext) => {
   const slug = context.params?.slug as string;
   const blog = await getBySlug(slug);
 
