a6a3989bf4ab94856d7d99909fccae735475c51c
Nguyen manh <nguyentienmanhhotboy@gmail.com>
2023-01-09 16:27:17 +0700

    fix

--- component/Paginate.js
+++ component/Paginate.js
@@ -27,7 +27,7 @@ const Paginate = ({ currentPage, setCurrentPage, totalPosts, postPerPage }) => {
 					</li>
 				))}
 				<li className={`page-item ${currentPage === totalPages && `disabled`}`}>
-					<button className="page-link block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={() => setCurrentPage(currentPage + 1)}>
+					<button className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={() => setCurrentPage(currentPage + 1)}>
 						&raquo;
 					</button>
 				</li>
